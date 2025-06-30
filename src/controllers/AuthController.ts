d40e91a8499c46e1738c02c3b7be059c3e2939bf
Daniyal Ahmed <da9033431@gmail.com>
2025-06-30 12:00:00 +0100

    logger and error handling for registration

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,23 +1,36 @@
-import type { Response } from 'express'
+import type { NextFunction, Response } from 'express'
 import type { registerUserRequest } from '../types/index.js'
 import type { UserService } from '../services/UserService.js'
+import type { Logger } from 'winston'
 
 export class AuthController {
-    constructor(private userService: UserService) {}
+    constructor(
+        private userService: UserService,
+        private logger: Logger
+    ) {}
 
-    async register(req: registerUserRequest, res: Response) {
+    async register(
+        req: registerUserRequest,
+        res: Response,
+        next: NextFunction
+    ) {
         const { firstName, lastName, email, password } = req.body
-
-        const user = await this.userService.createUser({
-            firstName,
-            lastName,
-            email,
-            password,
-        })
-
-        res.status(201).json({
-            message: 'Registration successful',
-            id: user.id,
-        })
+        this.logger.debug(`Registering user with email: ${email}`)
+        try {
+            const user = await this.userService.createUser({
+                firstName,
+                lastName,
+                email,
+                password,
+            })
+            this.logger.info(`User registered with id: ${user.id}`)
+            return res.status(201).json({
+                message: 'Registration successful',
+                id: user.id,
+            })
+        } catch (error) {
+            next(error)
+            return
+        }
     }
 }
