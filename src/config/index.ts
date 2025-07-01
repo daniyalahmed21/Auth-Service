aef7138d63ca696b096964ec3cd56bc8e082eda6
Daniyal Ahmed <da9033431@gmail.com>
2025-07-01 12:00:00 +0100

    crossenv setup

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,6 @@
 import { config } from 'dotenv'
 import path from 'path'
+import { fileURLToPath } from 'url'
 
 function required(key: string): string {
     const value = process.env[key]
@@ -9,16 +10,21 @@ function required(key: string): string {
     return value
 }
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 config({ path: path.resolve(__dirname, `../../.env.${process.env.NODE_ENV}`) })
 
-const { PORT, NODE_ENV, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT } = {
-    PORT: required('PORT'),
-    NODE_ENV: required('NODE_ENV'),
-    DB_USERNAME: required('DB_USERNAME'),
-    DB_PASSWORD: required('DB_PASSWORD'),
-    DB_HOST: required('DB_HOST'),
-    DB_PORT: required('DB_PORT'),
-}
+const { PORT, NODE_ENV, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } =
+    {
+        PORT: required('PORT'),
+        NODE_ENV: required('NODE_ENV'),
+        DB_USERNAME: required('DB_USERNAME'),
+        DB_PASSWORD: required('DB_PASSWORD'),
+        DB_HOST: required('DB_HOST'),
+        DB_NAME: required('DB_NAME'),
+        DB_PORT: required('DB_PORT'),
+    }
 
 export const Config = {
     PORT,
@@ -27,4 +33,5 @@ export const Config = {
     DB_PASSWORD,
     DB_HOST,
     DB_PORT,
+    DB_NAME,
 }
