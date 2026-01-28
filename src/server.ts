57fddf33eb8c140d45fd0ebc054b42819f36bd4b
Daniyal Ahmed <da9033431@gmail.com>
2026-01-28 12:00:00 +0000

    express and nodemon setup

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,15 @@
 import { Config } from './config/index.js'
+import app from './app.js'
 
-console.log(`Server is running on port ${Config.PORT}...`)
+function startServer() {
+    const PORT = Config.PORT || 3000
+    try {
+        app.listen(PORT, () => {
+            console.log(`Auth Service is running on port ${PORT}...`)
+        })
+    } catch (error) {
+        console.error('Error starting Auth Service:', error)
+    }
+}
+
+startServer()
