0a266e361e1a70219a8a6ee5f9774b7146a342b9
Daniyal Ahmed <da9033431@gmail.com>
2026-01-31 12:00:00 +0000

    Error Handling

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,31 @@
-import express from 'express'
+import express, {
+    type NextFunction,
+    type Request,
+    type Response,
+} from 'express'
+import { HttpError } from 'http-errors'
+import logger from './config/logger.js'
 
 const app = express()
 
 app.get('/', (req, res) => {
     res.send('Auth Service is running...')
+})
+
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.message)
+    const statusCode = err.statusCode || 500
+    res.status(statusCode).json({
+        error: [
+            {
+                type: err.name || 'InternalServerError',
+                message: err.message || 'An unexpected error occurred.',
+                path: req.originalUrl,
+                location: 'server',
+            },
+        ],
+    })
+    next()
 })
 
 export default app
