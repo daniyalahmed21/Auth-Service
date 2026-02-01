76a567b07606497531de1eecf6b5e342f5b8d241
Daniyal Ahmed <da9033431@gmail.com>
2026-02-01 18:27:57 +0000

    refactoring

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,23 @@
 //@ts-check
-import eslint from '@eslint/js';
-import { defineConfig } from 'eslint/config';
-import tseslint from 'typescript-eslint';
+import eslint from '@eslint/js'
+import { defineConfig } from 'eslint/config'
+import tseslint from 'typescript-eslint'
 
 export default defineConfig(
-  eslint.configs.recommended,
-  tseslint.configs.recommendedTypeChecked,
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+    eslint.configs.recommended,
+    tseslint.configs.recommendedTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        rules: {
+            'no-console': 'off',
+        },
     },
-    rules: {
-      'no-console': 'off',
-    },
-  },
-  {
-    ignores: ['dist', 'node_modules', 'eslint.config.mjs'],
-  }
-);
+    {
+        ignores: ['dist', 'node_modules', 'eslint.config.mjs'],
+    }
+)
